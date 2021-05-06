df6e3c7d72811dce7f86c8778b2e1db6d843e698
Pascal Krause <pascal.krause@sap.com>
2021-05-06 12:04:17 +0200

    feat: add missing tests for NeonBeeOptions

--- src/test/java/io/neonbee/NeonBeeOptionsTest.java
+++ src/test/java/io/neonbee/NeonBeeOptionsTest.java
@@ -1,18 +1,24 @@
 package io.neonbee;
 
 import static com.google.common.truth.Truth.assertThat;
+import static io.neonbee.NeonBeeProfile.ALL;
+import static io.neonbee.NeonBeeProfile.CORE;
+import static io.neonbee.NeonBeeProfile.WEB;
 import static io.neonbee.test.helper.FileSystemHelper.createTempDirectory;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import com.hazelcast.config.ClasspathXmlConfig;
+import com.hazelcast.config.Config;
 
+import io.neonbee.NeonBeeOptions.Mutable;
 import io.neonbee.test.helper.FileSystemHelper;
+import io.vertx.core.VertxOptions;
 import io.vertx.core.eventbus.EventBusOptions;
 
 class NeonBeeOptionsTest {
@@ -41,22 +47,6 @@ class NeonBeeOptionsTest {
         IllegalArgumentException exception =
                 assertThrows(IllegalArgumentException.class, () -> new NeonBeeOptions.Mutable().setInstanceName(""));
         assertThat(exception.getMessage()).isEqualTo("instanceName must not be empty");
-    }
-
-    @Test
-    @DisplayName("should throw an error, if event loop pool size is less then 1")
-    void checkThatEventLoopPoolSizeIsIsAtLeastOne() {
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
-                () -> new NeonBeeOptions.Mutable().setEventLoopPoolSize(0));
-        assertThat(exception.getMessage()).isEqualTo("eventLoopSize must be > 0");
-    }
-
-    @Test
-    @DisplayName("should throw an error, if worker pool size is less then 1")
-    void checkThatWorkerPoolSizeIsIsAtLeastOne() {
-        IllegalArgumentException exception =
-                assertThrows(IllegalArgumentException.class, () -> new NeonBeeOptions.Mutable().setWorkerPoolSize(0));
-        assertThat(exception.getMessage()).isEqualTo("workerPoolSize must be > 0");
     }
 
     @Test
@@ -113,16 +103,6 @@ class NeonBeeOptionsTest {
     }
 
     @Test
-    @DisplayName("Test config set correctly")
-    void checkClusterConfig() {
-        assertThat(new NeonBeeOptions.Mutable().setClusterConfigResource("hazelcast-local.xml").getClusterConfig())
-                .isInstanceOf(ClasspathXmlConfig.class);
-        ClasspathXmlConfig xmlConfig = (ClasspathXmlConfig) new NeonBeeOptions.Mutable()
-                .setClusterConfigResource("hazelcast-local.xml").getClusterConfig();
-        assertThat(xmlConfig.getNetworkConfig().getPort()).isEqualTo(20000);
-    }
-
-    @Test
     @DisplayName("Test server verticle port set correctly")
     void checkServerVerticlePort() {
         assertThat(new NeonBeeOptions.Mutable().getServerPort()).isNull();
@@ -132,11 +112,58 @@ class NeonBeeOptionsTest {
     @Test
     @DisplayName("Test server profiles set correctly")
     void checkProfiles() {
-        assertThat(new NeonBeeOptions.Mutable().setActiveProfileValues("CORE,WEB").getActiveProfiles())
-                .contains(NeonBeeProfile.CORE);
-        assertThat(new NeonBeeOptions.Mutable().setActiveProfileValues("CORE,WEB").getActiveProfiles())
-                .contains(NeonBeeProfile.WEB);
-        assertThat(new NeonBeeOptions.Mutable().setActiveProfileValues("anything").getActiveProfiles())
-                .contains(NeonBeeProfile.ALL);
+        Mutable opts = new NeonBeeOptions.Mutable().setActiveProfileValues("CORE,WEB");
+        assertThat(opts.getActiveProfiles()).containsExactly(CORE, WEB);
+        opts = new NeonBeeOptions.Mutable().setActiveProfiles(List.of(CORE, WEB));
+        assertThat(opts.getActiveProfiles()).containsExactly(CORE, WEB);
+        opts = new NeonBeeOptions.Mutable().setActiveProfileValues("anything");
+        assertThat(opts.getActiveProfiles()).containsExactly(ALL);
+    }
+
+    @Test
+    @DisplayName("Test event loop pool size getter and setter")
+    void testEventLoopPoolSizeGetterSetter() {
+        Mutable mutable = new NeonBeeOptions.Mutable();
+        assertThat(mutable.getEventLoopPoolSize()).isEqualTo(VertxOptions.DEFAULT_EVENT_LOOP_POOL_SIZE);
+        assertThat(mutable.setEventLoopPoolSize(10000).getEventLoopPoolSize()).isEqualTo(10000);
+    }
+
+    @Test
+    @DisplayName("should throw an error, if event loop pool size is less then 1")
+    void checkThatEventLoopPoolSizeIsIsAtLeastOne() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> new NeonBeeOptions.Mutable().setEventLoopPoolSize(0));
+        assertThat(exception.getMessage()).isEqualTo("eventLoopSize must be > 0");
+    }
+
+    @Test
+    @DisplayName("Test worker pool size getter and setter")
+    void testWorkerPoolSizeGetterSetter() {
+        Mutable mutable = new NeonBeeOptions.Mutable();
+        assertThat(mutable.getWorkerPoolSize()).isEqualTo(VertxOptions.DEFAULT_WORKER_POOL_SIZE);
+        assertThat(mutable.setWorkerPoolSize(10000).getWorkerPoolSize()).isEqualTo(10000);
+    }
+
+    @Test
+    @DisplayName("should throw an error, if worker pool size is less then 1")
+    void checkThatWorkerPoolSizeIsIsAtLeastOne() {
+        IllegalArgumentException exception =
+                assertThrows(IllegalArgumentException.class, () -> new NeonBeeOptions.Mutable().setWorkerPoolSize(0));
+        assertThat(exception.getMessage()).isEqualTo("workerPoolSize must be > 0");
+    }
+
+    @Test
+    @DisplayName("Test clusterConfig getter and setter")
+    void testClusterConfig() {
+        Mutable mutable = new NeonBeeOptions.Mutable();
+        Config defaultConfig = mutable.getClusterConfig();
+        assertThat(defaultConfig.getNetworkConfig().getPort()).isEqualTo(50000);
+
+        mutable.setClusterConfigResource("hazelcast-local.xml");
+        Config localConfig = mutable.getClusterConfig();
+        assertThat(localConfig.getNetworkConfig().getPort()).isEqualTo(20000);
+
+        mutable = new NeonBeeOptions.Mutable().setClusterConfig(localConfig);
+        assertThat(mutable.getClusterConfig().getNetworkConfig().getPort()).isEqualTo(20000);
     }
 }
