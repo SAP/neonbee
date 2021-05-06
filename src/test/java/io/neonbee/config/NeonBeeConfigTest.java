40b66db0ac8ea592ffa560b370d3e52a6a42edcc
Pascal Krause <pascal.krause@sap.com>
2021-05-06 11:21:49 +0200

    feat: add missing tests for NeonBeeConfig

--- src/test/java/io/neonbee/config/NeonBeeConfigTest.java
+++ src/test/java/io/neonbee/config/NeonBeeConfigTest.java
@@ -1,17 +1,62 @@
 package io.neonbee.config;
 
 import static com.google.common.truth.Truth.assertThat;
+import static io.neonbee.config.NeonBeeConfig.DEFAULT_EVENT_BUS_TIMEOUT;
+import static io.neonbee.config.NeonBeeConfig.DEFAULT_TIME_ZONE;
+import static io.neonbee.config.NeonBeeConfig.DEFAULT_TRACKING_DATA_HANDLING_STRATEGY;
 
+import java.lang.reflect.Method;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 
+import io.neonbee.test.base.NeonBeeTestBase;
+import io.neonbee.test.helper.WorkingDirectoryBuilder;
+import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
+import io.vertx.junit5.Timeout;
+import io.vertx.junit5.VertxTestContext;
 
-class NeonBeeConfigTest {
+class NeonBeeConfigTest extends NeonBeeTestBase {
+    private static final int DUMMY_EVENT_BUS_TIMEOUT = 1337;
+
+    private static final String DUMMY_TRACKING_DATA_HANDLING_STRATEGY = "Hodor";
+
+    private static final String DUMMY_TIME_ZONE = "Hammer Time";
+
+    private static final Map<String, String> DUMMY_EVENT_BUS_CODECS = Map.of("Random", "Codec");
+
+    private static final List<String> DUMMY_PLATFORM_CLASSES = List.of("Hodor");
+
+    private static final NeonBeeConfig DUMMY_NEONBEE_CONFIG =
+            new NeonBeeConfig().setEventBusTimeout(DUMMY_EVENT_BUS_TIMEOUT)
+                    .setTrackingDataHandlingStrategy(DUMMY_TRACKING_DATA_HANDLING_STRATEGY).setTimeZone(DUMMY_TIME_ZONE)
+                    .setEventBusCodecs(DUMMY_EVENT_BUS_CODECS).setPlatformClasses(DUMMY_PLATFORM_CLASSES);
+
+    @Override
+    protected WorkingDirectoryBuilder provideWorkingDirectoryBuilder(TestInfo testInfo, VertxTestContext testContext) {
+        if ("testLoad".equals(testInfo.getTestMethod().map(Method::getName).orElse(null))) {
+            return WorkingDirectoryBuilder.standard().setNeonBeeConfig(DUMMY_NEONBEE_CONFIG);
+        } else {
+            return super.provideWorkingDirectoryBuilder(testInfo, testContext);
+        }
+    }
+
+    @Test
+    @Timeout(value = 2, timeUnit = TimeUnit.SECONDS)
+    @DisplayName("should load NeonBeeConfig correctly from working dir")
+    void testLoad(Vertx vertx, VertxTestContext testContext) {
+        NeonBeeConfig.load(vertx).onComplete(testContext.succeeding(nbc -> {
+            testContext.verify(() -> isEqualToDummyConfig(nbc));
+            testContext.completeNow();
+        }));
+    }
 
     @Test
     @DisplayName("should read the trackingDataHandlingStrategy correctly")
@@ -37,8 +82,27 @@ class NeonBeeConfigTest {
     }
 
     @Test
-    @DisplayName("should have the correct default timezone")
-    void testDefaultTimeZone() {
-        assertThat(new NeonBeeConfig().getTimeZone()).isEqualTo(NeonBeeConfig.DEFAULT_TIME_ZONE);
+    @DisplayName("should have the correct default values")
+    void testDefaultValues() {
+        NeonBeeConfig defaultConfig = new NeonBeeConfig();
+        assertThat(defaultConfig.getEventBusTimeout()).isEqualTo(DEFAULT_EVENT_BUS_TIMEOUT);
+        assertThat(defaultConfig.getTrackingDataHandlingStrategy()).isEqualTo(DEFAULT_TRACKING_DATA_HANDLING_STRATEGY);
+        assertThat(defaultConfig.getTimeZone()).isEqualTo(DEFAULT_TIME_ZONE);
+        assertThat(defaultConfig.getEventBusCodecs()).isEmpty();
+        assertThat(defaultConfig.getPlatformClasses()).isEmpty();
+    }
+
+    @Test
+    @DisplayName("setters should work as expected")
+    void testSetters() {
+        isEqualToDummyConfig(DUMMY_NEONBEE_CONFIG);
+    }
+
+    private void isEqualToDummyConfig(NeonBeeConfig nbc) {
+        assertThat(nbc.getEventBusTimeout()).isEqualTo(DUMMY_EVENT_BUS_TIMEOUT);
+        assertThat(nbc.getTrackingDataHandlingStrategy()).isEqualTo(DUMMY_TRACKING_DATA_HANDLING_STRATEGY);
+        assertThat(nbc.getTimeZone()).isEqualTo(DUMMY_TIME_ZONE);
+        assertThat(nbc.getEventBusCodecs()).isEqualTo(DUMMY_EVENT_BUS_CODECS);
+        assertThat(nbc.getPlatformClasses()).isEqualTo(DUMMY_PLATFORM_CLASSES);
     }
 }
