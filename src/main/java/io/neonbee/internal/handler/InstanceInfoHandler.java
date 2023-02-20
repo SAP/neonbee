14f29a189a12bebb65803f6821e885077981bcfb
Sebastian Heid <sebastian.heid@sap.com>
2023-02-20 10:26:37 +0100

    chore: remove webserver name from X-Instance-Info header
    For security reasons, the webserver name should not be public, because
    this allows attackers to gather information about the application and
    potentially identify new attack surfaces. The NeonBee prefix in the
    X-Instance-Info header does not provide any benefit, and therefore
    can be removed.
    
    Closes #268

--- src/main/java/io/neonbee/internal/handler/InstanceInfoHandler.java
+++ src/main/java/io/neonbee/internal/handler/InstanceInfoHandler.java
@@ -5,6 +5,11 @@ import io.vertx.core.MultiMap;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.PlatformHandler;
 
+/**
+ * This Vert.x handler sets the "X-Instance-Info" header in the HTTP response, if not already set. The value of this
+ * header is set to a unique identifier for the NeonBee instance which is handling the request. This handler should be
+ * used in a Vert.x router to set the instance info header for all outgoing HTTP responses.
+ */
 public class InstanceInfoHandler implements PlatformHandler {
     static final String X_INSTANCE_INFO_HEADER = "X-Instance-Info";
 
