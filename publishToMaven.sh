cfc52d7a30901e509e3ff8a77fefe66c69381065
Benjamin Kernberger <benjamin.kernberger@sap.com>
2025-09-03 10:38:07 +0200

    feat: change to ossrh-staging-api

--- publishToMaven.sh
+++ publishToMaven.sh
@@ -10,3 +10,7 @@ export ORG_GRADLE_PROJECT_mavenCentralPassword=ossrh-jira-password
 
 cd "${SCRIPTPATH}"
 ./gradlew publishAllPublicationsToMavenCentralRepository
+
+BEARER=$(printf '%s:%s' "$ORG_GRADLE_PROJECT_mavenCentralUsername" "$ORG_GRADLE_PROJECT_mavenCentralPassword" | base64)
+
+curl -v -H "Authorization: Bearer $BEARER" -i -X POST 'https://ossrh-staging-api.central.sonatype.com/manual/upload/defaultRepository/io.neonbee'
