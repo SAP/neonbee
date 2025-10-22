f5852e82477bb846bc99a1712ab3cf1997b8e1fe
Michael Halberstadt <michael.halberstadt@sap.com>
2025-10-22 19:50:43 +0200

    fix: move requestEntity to AbstractEntityVerticle

--- src/main/java/io/neonbee/entity/EntityVerticle.java
+++ src/main/java/io/neonbee/entity/EntityVerticle.java
@@ -1,15 +1,7 @@
 package io.neonbee.entity;
 
-import static io.neonbee.internal.verticle.ConsolidationVerticle.ENTITY_TYPE_NAME_HEADER;
-import static io.vertx.core.Future.failedFuture;
-import static io.vertx.core.Future.succeededFuture;
-
-import org.apache.olingo.commons.api.edm.FullQualifiedName;
-
 import io.neonbee.data.DataContext;
-import io.neonbee.data.DataQuery;
 import io.neonbee.data.DataRequest;
-import io.neonbee.internal.verticle.ConsolidationVerticle;
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
 
@@ -24,31 +16,7 @@ public abstract class EntityVerticle extends AbstractEntityVerticle<EntityWrappe
      * @return a future to the data requested
      */
     public static Future<EntityWrapper> requestEntity(Vertx vertx, DataRequest request, DataContext context) {
-        FullQualifiedName entityTypeName = request.getEntityTypeName();
-        if (entityTypeName == null) {
-            throw new IllegalArgumentException(
-                    "A entity request must specify an entity type name to request data from");
-        }
-
-        /*
-         * TODO, as soon as having multiple verticle for an entity is not longer a corner case, I would recommend that
-         * we send the "qualifiedNames" in a header to the ConsolidationVerticle. Then it wouldn't be necessary to do
-         * the getVerticlesForEntityType call twice.
-         */
-        return getVerticlesForEntityType(vertx, entityTypeName).compose(qualifiedNames -> {
-            if (qualifiedNames.isEmpty()) {
-                return failedFuture("No verticle registered listening to entity type name "
-                        + entityTypeName.getFullQualifiedNameAsString());
-            } else if (qualifiedNames.size() == 1) {
-                return requestData(vertx, new DataRequest(qualifiedNames.get(0), request.getQuery()), context);
-            } else {
-                DataQuery query = request.getQuery().copy().setHeader(ENTITY_TYPE_NAME_HEADER,
-                        entityTypeName.getFullQualifiedNameAsString());
-                return requestData(vertx,
-                        new DataRequest(ConsolidationVerticle.QUALIFIED_NAME, query).setLocalOnly(true), context);
-            }
-        }).compose(entity -> entity instanceof EntityWrapper ? succeededFuture((EntityWrapper) entity)
-                : failedFuture("The result of entity verticle must be an EntityWrapper"));
+        return AbstractEntityVerticle.requestEntity(EntityWrapper.class, vertx, request, context);
     }
 
     /**
